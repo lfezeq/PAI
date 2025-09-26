403e318d31ce3c267a37aa4ba6f14ed1804b88d2
lfezeq <thedestruktor1337@gmail.com>
2025-09-26 13:49:22 +0200

    new excrs

---
+++ PHP/l5cw6.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$a = 0;
+while ($a <= 10) {
+    echo "$a <br> ";
+    $a++;
+}
+
+echo "<br>";
+
+$a = 10;
+while ($a >= 1) {
+    echo "$a <br> ";
+    $a--;
+}
+?>
+</body>
+</html>
+
---
+++ PHP/l5cw7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($x = 5; $x <=15; $x++) {
+        if ($x % 2 == 1) {
+        echo "$x <br>";
+        }
+    }
+    ?>
+</body> 
+</html>
